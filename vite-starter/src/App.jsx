fb1f99694a24fe20d7aaebdf306ec9a4d1b99c55
pithesun <wonjh0730@naver.com>
2024-07-23 00:39:26 +0900

    [2-03] 퀴즈 (checkbox-functionality)

--- vite-starter/src/App.jsx
+++ vite-starter/src/App.jsx
@@ -1,7 +1,28 @@
+import "./App.css";
+import { useState } from "react";
+
 function App() {
+  const [disabled, setDisabled] = useState(false);
+  const [buttonColor, setButtonColor] = useState("red");
+  const newButtonColor = buttonColor === "red" ? "blue" : "red";
+
   return (
     <div>
-      <h1>I'm gonna learn React Testing Library</h1>
+      <button
+        className={buttonColor}
+        disabled={disabled}
+        onClick={() => setButtonColor(newButtonColor)}
+      >
+        Change to {newButtonColor}
+      </button>
+      <br />
+      <input
+        type="checkbox"
+        id="disable-button-checkbox"
+        defaultChecked={disabled}
+        onChange={(e) => setDisabled(e.target.checked)}
+      />
+      <label htmlFor="disable-button-checkbox">Disable button</label>
     </div>
   );
 }
--- vite-starter/src/App.test.jsx
+++ vite-starter/src/App.test.jsx
@@ -1,8 +1,46 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
-test("App contains correct heading", () => {
+test("button click flow", () => {
   render(<App />);
-  const headingElement = screen.getByText(/learn react/i);
-  expect(headingElement).toBeInTheDocument();
+
+  // find an element with a role of button and text matching /blue/i
+  const buttonElement = screen.getByRole("button", {
+    name: /blue/i,
+  });
+
+  // expect the class to be red
+  expect(buttonElement).toHaveClass("red");
+
+  // click button
+  fireEvent.click(buttonElement);
+
+  // expect the class to be blue
+  expect(buttonElement).toHaveClass("blue");
+
+  // expect the button text to match /red/i
+  expect(buttonElement).toHaveTextContent(/red/i);
+});
+
+test("disable button checkbox flow", () => {
+  render(<App />);
+
+  const checkboxElement = screen.getByRole("checkbox", {
+    name: /disable button/i,
+  });
+
+  const buttonElement = screen.getByRole("button", {
+    name: /blue/i,
+  });
+
+  expect(checkboxElement).not.toBeChecked();
+  expect(buttonElement).toBeEnabled();
+
+  fireEvent.click(checkboxElement);
+
+  // expect(checkboxElement).toBeChecked();
+  expect(buttonElement).not.toBeEnabled();
+
+  fireEvent.click(checkboxElement);
+  expect(buttonElement).toBeEnabled();
 });
